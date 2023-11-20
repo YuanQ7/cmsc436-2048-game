add5ab201e191453a727644543714f69d6eb9d9a
YuanQ7 <ykq4237@gmail.com>
2023-11-20 00:16:11 -0500

    Started main activity xml design, and other stuffs

---
+++ app/src/main/java/com/example/finalproject/Game.kt
@@ -0,0 +1,11 @@
+package com.example.finalproject
+
+class Game {
+
+    // The game board for 2048. Each tile starts as an empty string.
+    val board = Array(4) {
+        Array(4) {
+            ""
+        }
+    }
+}
---
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var model: Game
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        model = Game()
+
+
+
+
+    }
+}
