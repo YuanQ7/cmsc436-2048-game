31792beb69b61bbd59bce545ac54f5ac49f16392
mson3476 <112913205+mson3476@users.noreply.github.com>
2023-12-07 01:23:03 -0500

    Max updated Game.kt
    Added makeMove() and isComplete()

--- app/src/main/java/com/example/finalproject/Game.kt
+++ app/src/main/java/com/example/finalproject/Game.kt
@@ -16,12 +16,13 @@ class Game {
     constructor( boardSize : Int ) {
         this.boardSize = boardSize
         newBoard(boardSize)
+        Log.w("test", "BoardSize: $boardSize")
     }
 
     // Creates a 2d array of shuffled numbers from 0 to 8 inclusive
     fun newBoard( size : Int ) {
-        var list = (0..size*size-1).toList().shuffled()
-        var idxZero = list.indexOf(0)
+        val list = (0..<size*size).toList().shuffled()
+        val idxZero = list.indexOf(0)
         zeroRowIndex = idxZero/size
         zeroColIndex = idxZero%size
         board = Array( size ) { i -> Array(size){ j -> list[size * i + j] } }
@@ -36,6 +37,13 @@ class Game {
         Log.w("asd", zeroColIndex.toString())*/
     }
 
+    // For testing
+    fun logBoard() : Unit {
+        for(i in board.indices) {
+            Log.w("test", board[i].contentToString())
+        }
+    }
+
     fun getBoardSize() : Int {
         return boardSize
     }
@@ -44,4 +52,47 @@ class Game {
         return board[row][col]
     }
 
+    // Attempts to move a piece in the given direction to the empty spot
+    // Returns true if the move was made, false if the move was not possible
+    fun makeMove(direction: String) : Boolean {
+        if (direction == "RIGHT" && zeroColIndex != 0) {
+            board[zeroRowIndex][zeroColIndex] = board[zeroRowIndex][zeroColIndex - 1]
+            board[zeroRowIndex][zeroColIndex - 1] = 0
+            zeroColIndex--
+        } else if (direction == "LEFT" && zeroColIndex != boardSize - 1) {
+            board[zeroRowIndex][zeroColIndex] = board[zeroRowIndex][zeroColIndex + 1]
+            board[zeroRowIndex][zeroColIndex + 1] = 0
+            zeroColIndex++
+        } else if (direction == "UP" && zeroRowIndex != boardSize - 1) {
+            board[zeroRowIndex][zeroColIndex] = board[zeroRowIndex + 1][zeroColIndex]
+            board[zeroRowIndex + 1][zeroColIndex] = 0
+            zeroRowIndex++
+        } else if (direction == "DOWN" && zeroRowIndex != 0) {
+            board[zeroRowIndex][zeroColIndex] = board[zeroRowIndex - 1][zeroColIndex]
+            board[zeroRowIndex - 1][zeroColIndex] = 0
+            zeroRowIndex--
+        } else {
+            return false
+        }
+        return true
+    }
+
+    // Checks if the puzzle has been solved
+    fun isComplete() : Boolean {
+        var num = 1
+        for (i in 0..<boardSize) {
+            for ( j in 0..<boardSize) {
+                // If at the bottom right corner, check that the value is 0
+                if (i == boardSize - 1 && j == boardSize - 1) {
+                    return board[i][j] == 0
+
+                // Else check that the values of the puzzle pieces are increasing incrementally
+                } else if (board[i][j] != num) {
+                    return false
+                }
+                num++
+            }
+        }
+        return true
+    }
 }
